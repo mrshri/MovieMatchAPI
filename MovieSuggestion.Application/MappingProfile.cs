ea8489188f1ea54049e23d7be209fa604c04b3e1
Shri Nivas <mrshri404@gmail.com>
2025-10-17 12:13:54 +0530

    Refactor API to use DTOs and integrate AutoMapper
    - Updated `MoviesController` to use `MovieCreateDTO` and `MovieDTO` for input/output, added validation, and improved error handling.
    - Introduced `MovieCreateDTO` and `MovieDTO` for data transfer.
    - Added `MappingProfile` to configure `AutoMapper` mappings.
    - Integrated `AutoMapper` into `MovieService` for entity-to-DTO mapping.
    - Updated `IMovieService` interface to reflect DTO usage.
    - Added `AutoMapper` package to both API and Application projects.
    - Improved separation of concerns and streamlined object mapping.

---
+++ MovieSuggestion.Application/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using MovieSuggestion.Application.DTOs;
+using MovieSuggestion.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieSuggestion.Application
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Movie, MovieDTO>();
+            CreateMap<MovieCreateDTO, Movie>();
+        }
+    }
+}
--- MovieSuggestion.Application/Services/MovieService.cs
+++ MovieSuggestion.Application/Services/MovieService.cs
@@ -1,4 +1,6 @@
-﻿using MovieSuggestion.Domain.Entities;
+﻿using AutoMapper;
+using MovieSuggestion.Application.DTOs;
+using MovieSuggestion.Domain.Entities;
 using MovieSuggestion.Infrastructure.Repositories.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -11,25 +13,30 @@ namespace MovieSuggestion.Application.Services
     public class MovieService : IMovieService
     {
         private readonly IMovieRepository _repo;
+        private readonly IMapper _mapper;
 
-        public MovieService(IMovieRepository movieRepository)
+        public MovieService(IMovieRepository movieRepository,IMapper mapper)
         {
             _repo = movieRepository;
+            _mapper = mapper;
         }
-        public async Task AddMovieAsync(Movie movie)
+        public async Task AddMovieAsync(MovieCreateDTO movieDto)
         {
+            var movie = _mapper.Map<Movie>(movieDto);
             await _repo.AddAsync(movie);
             await _repo.SaveChangesAsync();
         }
 
-        public async Task<IEnumerable<Movie>> GetAllMoviesAsync()
+        public async Task<IEnumerable<MovieDTO>> GetAllMoviesAsync()
         {
-            return await _repo.GetAllAsync();
+            var movies =  await _repo.GetAllAsync();
+            return _mapper.Map<IEnumerable<MovieDTO>>(movies);
         }
 
-        public async Task<Movie?> GetMovieByIdAsync(int id)
+        public async Task<MovieDTO?> GetMovieByIdAsync(int id)
         {
-           return await _repo.GetByIdAsync(id);
+           var movie = await _repo.GetByIdAsync(id);
+            return _mapper.Map<MovieDTO?>(movie);
         }
     }
 }
