da2c5eb4ad902fb99bdcb4cb5e851f817ad3187f
Shri Nivas <mrshri404@gmail.com>
2025-10-18 11:51:21 +0530

    Add global validation filter and FluentValidation support (#4)
    Introduced a `ValidationFilter` to handle model validation globally, returning detailed error messages for invalid requests.
    
    Integrated FluentValidation by adding `FluentValidation` and `FluentValidation.DependencyInjectionExtensions` packages.
    
    Created `MovieDTOValidator` to enforce validation rules for `MovieDTO` properties (`Title` and `Genre`).
    
    Updated `Program.cs` to register the `ValidationFilter` globally and included necessary namespaces.

---
+++ MovieSuggestion.Application/Validators/MovieDTOValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using MovieSuggestion.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieSuggestion.Application.Validators
+{
+    public class MovieDTOValidator : AbstractValidator<MovieDTO>
+    {
+        public MovieDTOValidator()
+        {
+            RuleFor(movie => movie.Title)
+                .NotEmpty().WithMessage("Tile is required.")
+                .Length(2, 100).WithMessage("Title must be between 2 and 100 characters.");
+
+            RuleFor(movie => movie.Genre)
+                .NotEmpty().WithMessage("Genre is required.")
+                .Length(2, 50).WithMessage("Genre must be between 2 and 50 characters.");
+        }
+    }
+}
